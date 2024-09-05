900318660c2580aaf9dab95d6a10ba8ee49500d5
VRX2314 <vanshgalaxy@gmail.com>
2024-09-05 17:32:55 +0530

    Init

---
+++ bot-agents/temp.py
@@ -0,0 +1,86 @@
+def temporary_job():
+    return """"About the Company:
+
+Fractal is one of the most prominent providers of Artificial Intelligence to Fortune 500® companies. Fractal's vision is to power every human decision in the enterprise, and bring AI, engineering, and design to help the world's most admired companies.
+
+Fractal's businesses include Crux Intelligence (AI driven business intelligence), Eugenie.ai (AI for sustainability), Asper.ai (AI for revenue growth management) and Senseforth.ai (conversational AI for sales and customer service). Fractal incubated Qure.ai, a leading player in healthcare AI for detecting Tuberculosis and Lung cancer.
+
+Fractal currently has 4000+ employees across 16 global locations, including the United States, UK, Ukraine, India, Singapore, and Australia. Fractal has been recognized as 'Great Workplace' and 'India's Best Workplaces for Women' in the top 100 (large) category by The Great Place to Work® Institute; featured as a leader in Customer Analytics Service Providers Wave™ 2021, Computer Vision Consultancies Wave™ 2020 & Specialized Insights Service Providers Wave™ 2020 by Forrester Research Inc., a leader in Analytics & AI Services Specialists Peak Matrix 2022 by Everest Group and recognized as an 'Honorable Vendor' in 2022 Magic Quadrant™ for data & analytics by Gartner Inc. For more information, visit fractal.ai
+
+Fractal’s businesses have been involved in multiple areas of A.I. research for the past several years and we have recently setup a A.I. Research Lab to focus on the most important problems in A.I. The A.I. Research Lab has worked extensively on the Foundation Model /GenAI ecosystem across a wide variety of areas.  The team has helped build in-house foundation (diffusion) models, has extensively worked on Large Language Model innovations and has built various aspects of Agentic systems that can lead to AGI. The team has access to inhouse GPU-cluster farm along with cloud GPU resources for solving complex A.I. problems.
+
+The A.I. Research Lab will focus on foundational aspects of A.I. research that push the boundaries of what is possible and move us towards a world with safe and effective AGI.
+
+Job Overview:
+
+We are looking for people who can explore new ideas, design and implement next-generation architectures/techniques in image/video, language, audio and multimodal space. You should be motivated to push the boundaries not just in state-of-the-art performance, but also in balancing performance in terms of inference speed and compute resource usage. You will collaborate with a multidisciplinary team of researchers, engineers, and data scientists to develop and deploy practical and scalable solutions to real-world challenges in areas such as domain based instruction fine-tuning LLMs, improving text to image and foundational models, building multi-modal foundation models, researching agentic ecosystems to name a few. Your work will have a direct impact on the development of our AI-driven products and services.
+
+Responsibilities:
+
+Conduct research and stay up to date with the latest advancements in Machine vision, NLP, Deep Learning, RL, Agentic systems and related fields.
+Design and develop novel and next generation of A.I. algorithms, models, and techniques to address specific problem domains
+Build A.I. Engineering pipelines for scaling A.I. models to internet scale
+Document research findings, methodologies, and experimental results in technical reports, blogs and papers.
+Publish research findings and contribute to conferences, workshops, and other scientific forums.
+Collaborate with engineering and business teams on model deployment and customised training respectively.
+Qualifications:
+
+Extensive understanding of advanced DL or generative architectures/techniques like diffusion architectures, attention models, LLMs, SAM, RLHF etc
+Have a track record of coming up with new ideas or improving upon existing ideas in deep learning quickly, demonstrated by accomplishments such as first author publications or published/deployed projects or impactful blogs.
+Ability to efficiently work on a modern deep learning stack, such as Python, PyTorch, and GPU-enabled compute, eg writing non-trivial custom architecture in Pytorch.
+Ability to iterate quickly on open-source code-bases with attention to backwards compatibility, usability, and readability. 
+Creative, fast-paced executor, detail-oriented, eager to learn, acquire new skills. 
+Ability to work effectively in a collaborative team environment and take ownership.
+Strong belief in a positive-sum game, valuing cooperation and collaboration over zero-sum competition.
+If you like wild growth and working with happy, enthusiastic over-achievers, you'll enjoy your career with us!
+
+Not the right fit?  Let us know you're interested in a future opportunity by clicking Introduce Yourself in the top-right corner of the page or create an account to set up email alerts as new job postings become available that meet your interest!
+"""
+
+
+def temporary_resume():
+    return """NAME 
+Vansh Shah
+
+SKILLS
+Machine Learning - Data Wrangling, Pre-processing, NumPy, Pandas, SciPy Kit Pipelines, ML Models and Optimization
+Deep Vision - Image and Video Processing, PyTorch, TensorFlow, Neural Networks, 3D Volumetric Processing
+Generative AI - Langchain, Llamaindex, Llama.cpp, Prompt Engineering, RAG, Finetuning, Multi-Agent Graph Systems
+DevOps & Web Dev - Docker, Linux server, Git, Flask, MongoDB, Express.js, React.js, Node.js
+
+EXPERIENCE
+1. Reliance Jio Platforms Ltd. as Generative AI Intern - 05/2024 – 09/2024
+TASKS PERFORMED	                                                                 
+Developing a multi-agent tool enabled system using Langchain and Open-source Large Language Models.
+Aiding in Open-source migration of RAG assisted LLM system to reduce overall system operation costs.
+Researching methodologies and architectures for deploying Large Language Models.
+
+2. Reliance Jio Platforms Ltd. as Machine Learning / Deep Learning Intern - 01/2023 – 06/2023
+TASKS PERFORMED	                                   
+Developed deep learning model for facial recognition image quality assessment, resulting in a 97% F1 Score and reduced false positives through an extensive iterative process using PyTorch and Fast.AI. 
+Collaborated with the team on curating spoof detection dataset for facial recognition using computer vision, contributing to improved accuracy of recognition systems. 
+Gained experience training and evaluating deep learning models with complex data pre-processing pipelines through coursework and research initiatives.
+
+KEY PROJECTS
+1. AI Based 3D Scene Reconstruction - Neural Radiance Fields - 2024                                                                                                  
+Explored structure from motion and LiDAR assisted data collection techniques to capture 3D spatial data. 
+Researched concepts such as Ray Tracing and Volumetric rendering.
+Leveraged open-source tools such as NeRF Studio for observability and visualization.
+
+2. Facial Quality Index - 2023                                                                                                         
+Curated and labeled custom facial image dataset integrating synthetic and publicly available datasets.
+Applied computer vision based augmentation to simulate real-world image quality degradation.
+Developed an image classification model segregating image into usable or unusable based on quality metrics.
+
+3. Retinal Vessel Segmentation - 2023		                                                                                                      
+Implemented UNET architecture-based model for segmentation of intricate retinal vessels with an 89% F1 Score.
+Compiled a retinal tomography scan dataset using openly available datasets such as DRIVE, FIVES.
+Created a user interface for model inference using Streamlit.
+
+EDUCATION
+B.tech Computer Engineering - 9.6 / 10 CGPA - Expected: 2025
+
+CERTIFICATIONS
+IBM AI Specialization: Machine Learning with Python (Honors), Introduction to Deep Learning and Neural Networks with Keras, Introduction to Computer Vision and Image Processing, Deep Neural Networks with PyTorch (Honors), Building Deep Learning Models with TensorFlow, AI Capstone Project with Deep Learning
+LinkedIn Learning Essential Training: HTML, CSS, JavaScript, React.js, SQL, Python
+Others: Generative AI with Large Language Models, Version Control with Git, Forage//BCG GenAI Job Simulation"""
