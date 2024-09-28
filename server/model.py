72e0b2ebd361788ae6c8eeaa2dee9a530798ee3b
Siddh Sanghvi <Ecuas235>
2024-09-28 22:01:53 +0530

    Added Page Navigation

--- server/model.py
+++ server/model.py
@@ -1,6 +1,18 @@
+import numpy as np
+
 from crawler import Crawler
 from agents import CondensorAgent, EvaluatorAgent, CondensorEvaluatorGraph, HybridAgent
 from playwright.async_api import async_playwright
+from warnings import filterwarnings
+
+from temp import temporary_resume
+
+from langchain_groq import ChatGroq
+import asyncio
+
+from tqdm import tqdm
+filterwarnings('ignore')
+
 
 
 class LLMCrawler(Crawler):
@@ -13,18 +25,20 @@ class LLMCrawler(Crawler):
             self.condensor, self.evaluator, self.hybrid
         )
 
-    async def scrape(self, hybrid: bool = False):
+    async def scrape(self, hybrid: bool = True):
         async with async_playwright() as p:
             ctx = 0
             await self._load_browser(p)
             await self._load_page()
+            
+            len_listings = 3
 
-            for _ in range(self.pages):
+            while len_listings != ctx:
                 job_elements = await self.page.query_selector_all(
                     ".jobTitle.css-198pbd.eu4oa1w0"
                 )
-
-                for job in job_elements:
+                
+                for job in tqdm(job_elements):
                     scraped_job = await self.scrape_indeed(job)
                     if hybrid:
                         response = self.condensor_evaluator_graph.execute_hybrid_graph(
@@ -34,19 +48,23 @@ class LLMCrawler(Crawler):
                         response = self.condensor_evaluator_graph.execute_graph(
                             f"{scraped_job}"
                         )
-
+                    # ctx += 1
+                    # if ctx > 2:
+                    #     break
                     yield f"{response}"
-
+                    await asyncio.sleep(np.random.choice(np.arange(1,3, 0.5)))
                     ctx += 1
-                    if ctx > 2:
+                    if len_listings == ctx:
+                        print(len_listings)
                         break
 
-                    if self.pages > 1:
-                        next_button = await self.page.query_selector(
-                            'a[data-testid="pagination-page-next"]'
-                        )
-                        if next_button:
-                            await next_button.click()
-                            await self.page.wait_for_timeout(5000)
-                        else:
-                            break
+                next_button = await self.page.query_selector(
+                    'a[data-testid="pagination-page-next"]'
+                )
+                if next_button:
+                    await next_button.click()
+                    await self.page.wait_for_timeout(5000)
+                else:
+                    break
+
+    
